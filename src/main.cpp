60e76cab3538f5d07124d370d79cac28a6857e03
Poommitol Chaicherdkiat <cpoommitol@gmail.com>
2017-12-25 14:04:05 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <MsTimer2.h>
+
+const int led_pin = LED_BUILTIN; // 1.0 built in LED pin var
+
+void flash()
+{
+    static boolean output = HIGH;
+    digitalWrite(led_pin, output);
+    output = !output;
+}
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+
+    pinMode(led_pin, OUTPUT);
+
+    MsTimer2::set(1000, flash); // 500ms period
+    MsTimer2::start();
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    Serial.println("TEST STRING");
+    delay(100);
+}
